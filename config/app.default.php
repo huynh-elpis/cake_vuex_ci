44163a942041971a9f1987d0f18fcea2e24d6952
huynh-primelabo <sy.huynh@primelabo.com.vn>
2018-05-09 17:09:43 +0700

    fix 3

--- config/app.default.php
+++ config/app.default.php
@@ -229,11 +229,12 @@ return [
              * the following line and set the port accordingly
              */
             //'port' => 'non_standard_port_number',
-            'username' => 'my_app',
-            'password' => 'secret',
-            'database' => 'my_app',
+            'username' => 'root',
+            'password' => '',
+            'database' => 'startuphub',
             'encoding' => 'utf8',
             'timezone' => 'UTC',
+
             'flags' => [],
             'cacheMetadata' => true,
             'log' => false,
@@ -270,9 +271,9 @@ return [
             'host' => '127.0.0.1',
             'username' => 'root',
             'password' => '',
-            'database' => 'AOU',
+            'database' => 'startuphub',
             'encoding' => 'utf8',
-            'timezone' => 'Asia/Tokyo',
+            'timezone' => 'UTC',
             'cacheMetadata' => true,
             'quoteIdentifiers' => false,
             'log' => false,
